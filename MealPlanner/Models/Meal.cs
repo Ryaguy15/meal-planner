080dee3485a3d988d46874e6ed14df6d24d770fb
Ryan Philipps <raphilip@mtu.edu>
2023-01-23 13:55:20 -0700

    Made Id a nullable type

--- MealPlanner/Models/Meal.cs
+++ MealPlanner/Models/Meal.cs
@@ -3,7 +3,7 @@ namespace MealPlanner.Models
 {
     public class Meal 
     {
-        public int Id {get; set;}
+        public int? Id {get; set;}
         public string Name {get;set;}
 
         public ICollection<Ingredient> Ingredients {get; set;}
